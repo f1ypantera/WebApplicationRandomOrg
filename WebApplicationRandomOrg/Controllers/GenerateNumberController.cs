47be75922b5e7dfe14380a8ae950429e1a4dfe5f
Vadym Tselikin <flyypaantera@gmail.com>
2018-10-04 13:05:20 +0300

    Work

--- WebApplicationRandomOrg/Controllers/GenerateNumberController.cs
+++ WebApplicationRandomOrg/Controllers/GenerateNumberController.cs
@@ -3,28 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebApplicationRandomOrg.Models;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace WebApplicationRandomOrg.Controllers
 {
-    public class HomeController : Controller
+    public class GenerateNumberController : Controller
     {
-
-        WebAppDbContext db = new WebAppDbContext();
-
-      
+        [Authorize]
         public ActionResult Index()
         {
-            
             return View();
         }
-
-       
-      
-
-
-
     }
 }
