7798a53bb3a6e2483252c31302beacc2456a33f1
Vadym Tselikin <flyypaantera@gmail.com>
2018-10-06 15:31:48 +0300

    update workwell

--- WebApplicationRandomOrg/Controllers/UserAccountssController.cs
+++ WebApplicationRandomOrg/Controllers/UserAccountssController.cs
@@ -3,34 +3,33 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebApplicationRandomOrg.Models;
-using System.ComponentModel.DataAnnotations;
 
 namespace WebApplicationRandomOrg.Controllers
 {
-    [Authorize(Users = "Ira")]
-    public class UserAccountsController : Controller
+    public class UserAccountssController : Controller
     {
-
         private WebAppDbContext db = new WebAppDbContext();
 
-        // GET: UserAccounts
-        public ActionResult Index()
+        // GET: UserAccountss
+        public async Task<ActionResult> Index()
         {
-            return View(db.UserAccounts.ToList());
+            var userAccounts = db.UserAccounts.Include(u => u.Role);
+            return View(await userAccounts.ToListAsync());
         }
 
-        // GET: UserAccounts/Details/5
-        public ActionResult Details(int? id)
+        // GET: UserAccountss/Details/5
+        public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            UserAccount userAccount = db.UserAccounts.Find(id);
+            UserAccount userAccount = await db.UserAccounts.FindAsync(id);
             if (userAccount == null)
             {
                 return HttpNotFound();
@@ -38,67 +37,72 @@ namespace WebApplicationRandomOrg.Controllers
             return View(userAccount);
         }
 
-        // GET: UserAccounts/Create
+        // GET: UserAccountss/Create
         public ActionResult Create()
         {
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName");
             return View();
         }
 
-        // POST: UserAccounts/Create
-      
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "AccountId,UserName,Email,Password,PasswordConfirm,Name,Surname,Year")] UserAccount userAccount)
-        {
-            if (ModelState.IsValid)
-            {
-                db.UserAccounts.Add(userAccount);
-                db.SaveChanges();
-                return RedirectToAction("Index","Home");
-            }
-
-            return View(userAccount);
-        }
-
-        // GET: UserAccounts/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            UserAccount userAccount = db.UserAccounts.Find(id);
-            if (userAccount == null)
-            {
-                return HttpNotFound();
-            }
-            return View(userAccount);
-        }
-
-        // POST: UserAccounts/Edit/5
+        // POST: UserAccountss/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "AccountId,UserName,Email,Password,PasswordConfirm,Name,Surname,Year")] UserAccount userAccount)
+        public async Task<ActionResult> Create([Bind(Include = "AccountId,UserName,Email,Password,PasswordConfirm,Name,Surname,Year,RoleID")] UserAccount userAccount)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(userAccount).State = EntityState.Modified;
-                db.SaveChanges();
+                db.UserAccounts.Add(userAccount);
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
+
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName", userAccount.RoleID);
             return View(userAccount);
         }
 
-       
-        public ActionResult Delete(int? id)
+        // GET: UserAccountss/Edit/5
+        public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            UserAccount userAccount = db.UserAccounts.Find(id);
+            UserAccount userAccount = await db.UserAccounts.FindAsync(id);
+            if (userAccount == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName", userAccount.RoleID);
+            return View(userAccount);
+        }
+
+        // POST: UserAccountss/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "AccountId,UserName,Email,Password,PasswordConfirm,Name,Surname,Year,RoleID")] UserAccount userAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userAccount).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName", userAccount.RoleID);
+            return View(userAccount);
+        }
+
+        // GET: UserAccountss/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserAccount userAccount = await db.UserAccounts.FindAsync(id);
             if (userAccount == null)
             {
                 return HttpNotFound();
@@ -106,14 +110,14 @@ namespace WebApplicationRandomOrg.Controllers
             return View(userAccount);
         }
 
-       
+        // POST: UserAccountss/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            UserAccount userAccount = db.UserAccounts.Find(id);
+            UserAccount userAccount = await db.UserAccounts.FindAsync(id);
             db.UserAccounts.Remove(userAccount);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
