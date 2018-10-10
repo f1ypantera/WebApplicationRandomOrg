496e012538252f74d8c306334a29c53eea25f2da
Vadym Tselikin <flyypaantera@gmail.com>
2018-10-10 17:29:05 +0300

    10.102018
    workwell

--- WebApplicationRandomOrg/Controllers/GenerateNumberController.cs
+++ WebApplicationRandomOrg/Controllers/GenerateNumberController.cs
@@ -4,21 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplicationRandomOrg.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApplicationRandomOrg.Services;
 
 namespace WebApplicationRandomOrg.Controllers
 {
     [Authorize]
     public class GenerateNumberController : Controller
     {
+        WebAppDbContext db = new WebAppDbContext();
         
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult RandomInteger()
+
+        [HttpGet]
+        public ActionResult RandomIntegerr()
         {
-      
             return View();
         }
+        [HttpPost]
+        public ActionResult RandomIntegerr(int min,int max)
+        {
+            Random rnd = new Random();
+            int res = rnd.Next(min, max);
+            ViewBag.output = res;
+            
+        
+           var currentUserName = HttpContext.User.Identity.Name;
+           var currentUser = db.UserAccounts.SingleOrDefault((u) => u.UserName == currentUserName);
+
+            var result = new Result()
+            {
+                OutPutResult = res.ToString(),
+                UserAccount = currentUser,
+                RequestType = db.RequestTypes.SingleOrDefault((t) => t.RequestTypeID == 1)
+            };
+
+            db.Results.Add(result);
+            db.SaveChanges();
+            
+
+            return View();
+        }
+       
+
     }
 }
---
+++ WebApplicationRandomOrg/Services/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationRandomOrg.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Web.Security;
+using System.Net;
+
+namespace WebApplicationRandomOrg.Services
+{
+    public class AccountService
+    {
+        WebAppDbContext db = new WebAppDbContext();
+
+        public UserAccount GetCurrentUser()
+        {
+            var user = HttpContext.Current.User.Identity.Name;
+            var currentUser  = db.UserAccounts.SingleOrDefault((a) => a.UserName == user);
+            return currentUser;
+            
+        }
+    }
+}
