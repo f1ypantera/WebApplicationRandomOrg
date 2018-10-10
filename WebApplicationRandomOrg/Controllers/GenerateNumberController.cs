2dd5633ad2fe99164b9248b6b65cd61a8c11db2f
Vadym Tselikin <flyypaantera@gmail.com>
2018-10-10 13:42:19 +0300

    10.10.2018
    update

--- WebApplicationRandomOrg/Controllers/GenerateNumberController.cs
+++ WebApplicationRandomOrg/Controllers/GenerateNumberController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationRandomOrg.Models;
 
 namespace WebApplicationRandomOrg.Controllers
 {
+    [Authorize]
     public class GenerateNumberController : Controller
     {
-        [Authorize]
+        
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult RandomInteger()
+        {
+      
             return View();
         }
     }
